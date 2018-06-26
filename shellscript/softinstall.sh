8ea0c0c55837c6187f9056fb40908b789b8409fb
JinHui <380663606@qq.com>
2018-06-26 11:24:48 +0800

    adjust the dir tree

---
+++ shellscript/softinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "安装git"
+sudo apt-get -y install git
+echo "安装svn"
+sudo apt-get -y install subversion
+echo "安装vim"
+sudo apt-get -y install vim
+echo "安装onie相关环境"
+sudo apt-get -y install bison
+sudo apt-get -y install flex
+sudo apt-get -y install texinfo
+sudo apt-get -y install help2man
+sudo apt-get -y install libpopt-dev
+sudo apt-get -y install efivar
+sudo apt-get -y install libefivar-dev
+sudo apt-get -y install libnss3
+sudo apt-get -y install libnss3-dev
+sudo apt-get -y install libnss3-tools
+sudo apt-get -y install autoconf
+sudo apt-get -y install gperf
+sudo apt-get -y install gawk
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install openssl
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libsigc++-2.0-dev
+sudo apt-get -y install uuid
+sudo apt-get -y install uuid-dev
+sudo apt-get -y install uuidcdef
+sudo apt-get -y install uuid-runtime
